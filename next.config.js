84a47b75a7d65778dbc9519cb9a4358b2efe5f28
Kuro091 <lcminhhp@gmail.com>
2022-06-24 01:50:06 +0700

    cleanup

--- next.config.js
+++ next.config.js
@@ -5,30 +5,10 @@ const nextConfig = {
     return [
       {
         source: '/:path*',
-        destination: 'https://zuyiohcxpuyezfuhatvo.supabase.co/:path*',
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://online-notepad-kuro091.vercel.app/, https://online-notepad-one.vercel.app/, https://online-notepad-git-main-kuro091.vercel.app/" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
+        destination: 'https://zuyiohcxpuyezfuhatvo.supabase.co/:path*'
       },
     ]
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://online-notepad-kuro091.vercel.app/, https://online-notepad-one.vercel.app/, https://online-notepad-git-main-kuro091.vercel.app/" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
